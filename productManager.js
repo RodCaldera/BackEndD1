85a0248073d727035e57c9b6377565577664fb27
Rodrigo Caldera <rodrigo_caldera@hotmail.com>
2023-07-31 13:07:57 -0600

    Se agregó manejo de archivos

--- productManager.js
+++ productManager.js
@@ -3,6 +3,9 @@
 
 const fs= require(fs);
 
+//Creamos el archivo
+fs.writeFileSync('products.json')     
+
 //Primer Código Actualizado
 
 alert('¡Vamos a empezar!')
@@ -10,6 +13,7 @@ class product{
     constructor(){
         this.product=[];
         this.id=0
+        this.path=this.id.json
     }
     
     addProduct(newProduct){
@@ -25,22 +29,56 @@ class product{
         this.product.push(newProduct);
         console.log('New product has been added:', title)
 
+        //El producto va al archivo
+        let stringifiedProduct= JSON.stringify(newProduct, undefined, 2)
+        fs.appendFileSync(this.path, stringifiedProduct,'utf-8')
     }
 
     getProducts(){
-        return this.product
+        let products = JSON.parse(fs.readFileSync(this.path, 'utf-8'))
+
+        /*return this.product*/
+        console.log(products)   
     }
 
     getProductById(id){
-        const findProduct = this.product.find(find=> product.id === id)
-        if (findProduct==true){
-            return findProduct
-        } 
-        else {
-            throw new Error ('Product not found')
+        const checkFile=fs.fileExistsSync(this.path)
+            if (checkFile==true){
+            const foundFile= JSON.parse(checkFile, 'utf-8')
+            console.log(foundFile)
+            } 
+            else {
+                throw new Error ('Product not found')
         }         
     }
-}        
+
+    deleteProduct(id){
+        const checkFile=fs.fileExistsSync(this.path)
+            if (checkFile==true){
+                fs.unlinkSync(checkfile)            } 
+            else {
+                throw new Error ('Product not found')
+        }         
+    }
+
+    updateProduct(id){
+        const {title, description, price, thumbnail, code, stock} = newProduct;
+        if(!title || !description || !price || !thumbnail || !code || !stock){
+            console.log('Error: product is incomplete');
+        }
+
+        
+        this.product.push(newProduct);
+        console.log('Product has been updated:', title)
+
+        //El producto va al archivo
+        let stringifiedProduct= JSON.stringify(newProduct, undefined, 2)
+        fs.writeFileSync(this.path, stringifiedProduct,'utf-8')
+    }
+    }
+    
+
+     
 
 
 const newProduct= new product();
@@ -56,5 +94,3 @@ console.log('Product found:', newProduct)
 
 newProduct.getProductById(4)
 console.log('Product found:', newProduct)
-
-//Agregar segunda entrega
